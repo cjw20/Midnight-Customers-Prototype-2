ba5e7b5d0023f51ed2f3f3a8fa9e531821b86194
cjw20 <cjw20@iu.edu>
2021-02-01 14:03:40 -0500

    Created game control object

---
+++ Midnight Customers Prototype 2/Assets/Scripts/GameControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl control;
+    void Awake()
+    {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
