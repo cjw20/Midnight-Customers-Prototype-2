8e68c8875c2d336c4093dea247a4e86bf1eafc91
cjw20 <cjw20@iu.edu>
2021-02-18 13:08:29 -0500

    Created conversation script that stores dialogue

---
+++ Midnight Customers Prototype 2/Assets/Scripts/Conversation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conversation : MonoBehaviour
+{
+
+    public string customerGreeting;
+    public string[] greetingResponses; //order important! A, b c etc.
+    public string[] customerMessages1;
+    public string[] responses1;
+    public string[] customerMessages2;
+    public string[] responses2;
+    public string[] customerMessages3;
+    public string[] responses3;
+    public string[] customerMessages4;
+    public string[] responses4;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
