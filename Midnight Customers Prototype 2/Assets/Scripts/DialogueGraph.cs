b92113c9e362687f9845cf90dadf25b3ad5e81a1
cjw20 <cjw20@iu.edu>
2021-02-18 14:19:03 -0500

    Created start node for dialogue system

--- Midnight Customers Prototype 2/Assets/Scripts/DialogueGraph.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/DialogueGraph.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor;
+using UnityEngine.UIElements;
 
 public class DialogueGraph : GraphViewEditorWindow
 {
-
+    DialogueGraphView _graphView;
 
     [MenuItem("Graph/ DialogueGraph")]
     public static void OpenDialogueGraphWindow()
     {
         var window = GetWindow<DialogueGraph>();
         window.titleContent = new GUIContent("Dialogue Graph");
+    }
+
+    private void OnEnable()
+    {
+        _graphView = new DialogueGraphView
+        {
+            name = "Dialogue Graph"
+        };
+
+        _graphView.StretchToParentSize();
+        rootVisualElement.Add(_graphView);
+    }
+
+    private void OnDisable()
+    {
+        rootVisualElement.Remove(_graphView);
     }
 }
--- Midnight Customers Prototype 2/Assets/Scripts/DialogueGraphView.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/DialogueGraphView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine.UIElements;
+using System;
 
 public class DialogueGraphView : GraphView
 { 
@@ -11,5 +12,22 @@ public class DialogueGraphView : GraphView
         this.AddManipulator(new ContentDragger());
         this.AddManipulator(new SelectionDragger());
         this.AddManipulator(new RectangleSelector());
+
+        AddElement(GenerateEntryPointNode());
+    }
+
+    DialogueNode GenerateEntryPointNode()
+    {
+        var node = new DialogueNode
+        {
+            title = "START",
+            GUID = Guid.NewGuid().ToString(),
+            dialogueText = "ENTRYPOINT",
+            entryPoint = true,
+        };
+
+        node.SetPosition(new Rect(100, 200, 100, 150));
+
+        return node;
     }
 }
