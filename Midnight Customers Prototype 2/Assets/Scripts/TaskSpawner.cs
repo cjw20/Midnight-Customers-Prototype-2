d739696c9d06943f986acaff3c50afe1c51dd50a
cjw20 <cjw20@iu.edu>
2021-04-26 14:30:47 -0400

    Fixed issues with task spawning

--- Midnight Customers Prototype 2/Assets/Scripts/TaskSpawner.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/TaskSpawner.cs
@@ -7,7 +7,7 @@ public class TaskSpawner : MonoBehaviour
     [SerializeField] GameObject mopTrigger;
     
     public List<GameObject> mopSpawns;
-    Transform spawn;
+    List<int> usedSpawns = new List<int>();
 
     // Start is called before the first frame update
     void Start()
@@ -24,19 +24,29 @@ public class TaskSpawner : MonoBehaviour
     public void NewDayTasks()
     {
         //logic to decide which tasks to choose here
-
+        List<int> usedSpawns = new List<int>(); //clears used spawns for tasks
         SpawnMopTask(2);
     }
 
     void SpawnMopTask(int amount)
     {
-        int random = Random.Range(1, 5);
+        int random = Random.Range(0, mopSpawns.Count);
         for(int i = 0; i < amount; i++)
         {
-            spawn = GameObject.Find("Mop " + random.ToString()).transform;
-            //Debug.Log(mopSpawns.Count);
-            Instantiate(mopTrigger, spawn);
-            random = Random.Range(1, 5);
+            Instantiate(mopTrigger, mopSpawns[random].transform);
+            usedSpawns.Add(random);
+            random = Random.Range(0, mopSpawns.Count);
+            while (usedSpawns.Contains(random)) //makes sure tasks dont spawn on each other
+            {
+                random++;
+                if(random > mopSpawns.Count - 1)
+                {
+                    random = 0; //makes sure random doesnt go outside list
+                }
+                //probably should find different way to do this later
+            }
         }
+
+        
     }
 }
