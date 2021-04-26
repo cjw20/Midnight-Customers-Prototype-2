71f33a2bc956d0121673976a37db61b301441e08
cjw20 <cjw20@iu.edu>
2021-04-26 14:57:03 -0400

    Created template for interactables in store

---
+++ Midnight Customers Prototype 2/Assets/Scripts/Interactable.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour
+{
+    bool inRange;
+    bool interacting;
+
+    [SerializeField] string[] messages;
+    public int progress;
+
+    GameObject thoughtText;
+    GameObject thoughtWindow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thoughtWindow = GameObject.Find("Thought Window");
+        thoughtText = GameObject.Find("Thought Text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.E))
+        {
+            if (inRange && !interacting)
+            {
+                interacting = true;
+                StartCoroutine(DisplayMessage(messages[progress]));
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inRange = false;
+        }
+    }
+
+    public void EndInteract()
+    {
+        thoughtWindow.SetActive(false);
+
+        progress++;
+        if(progress > messages.Length - 1)
+        {
+            progress--;
+        }
+    }
+
+    IEnumerator DisplayMessage(string message)
+    {
+        thoughtWindow.SetActive(true);
+        Text newThought = thoughtText.GetComponent<Text>();
+        newThought.text = "";
+        foreach (char letter in message.ToCharArray())
+        {
+            newThought.text += letter;
+            yield return new WaitForSeconds(0.05f); 
+        }
+        yield return new WaitForSeconds(1f); //waits a bit before dismissing thought
+
+        EndInteract();
+        yield break;
+    }
+}
