96b100e0ad93aacd81f61ee43018fbdd7979634e
cjw20 <cjw20@iu.edu>
2021-09-16 15:59:11 -0400

    task spawner now also spawns shelf games

--- Midnight Customers Prototype 2/Assets/Scripts/TaskSpawner.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/TaskSpawner.cs
@@ -5,17 +5,28 @@ using System.Linq;
 public class TaskSpawner : MonoBehaviour
 {
     [SerializeField] GameObject mopTrigger;
+    [SerializeField] GameObject shelfTrigger; //maybe make array once variations are added
     
-    public List<GameObject> mopSpawns;
+    public List<GameObject> mopSpawns; //make transform later
+    public List<Transform> shelfSpawns;
     List<int> usedSpawns = new List<int>();
+    List<GameObject> tasksInStore = new List<GameObject>();
+    public int unfinishedTasks; //number of spawned tasks that are unfinished
 
     
 
     public void NewDayTasks()
     {
+
+
         //logic to decide which tasks to choose here
         List<int> usedSpawns = new List<int>(); //clears used spawns for tasks
-        SpawnMopTask(2);
+
+        //spawn 1-2 of each task each day
+        int random = Random.Range(1, 2);
+        SpawnMopTask(random);
+        random = Random.Range(1, 2); 
+        SpawnShelfTask(random);
     }
 
     void SpawnMopTask(int amount)
@@ -23,7 +34,8 @@ public class TaskSpawner : MonoBehaviour
         int random = Random.Range(0, mopSpawns.Count);
         for(int i = 0; i < amount; i++)
         {
-            Instantiate(mopTrigger, mopSpawns[random].transform);
+            GameObject task = Instantiate(mopTrigger, mopSpawns[random].transform);
+            tasksInStore.Add(task);
             usedSpawns.Add(random);
             random = Random.Range(0, mopSpawns.Count);
 
@@ -46,5 +58,35 @@ public class TaskSpawner : MonoBehaviour
         }
 
         
+    }
+
+    void SpawnShelfTask(int amount)
+    {
+        //same as mop logic for now
+        int random = Random.Range(0, shelfSpawns.Count);
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject task = Instantiate(shelfTrigger, shelfSpawns[random]);
+            tasksInStore.Add(task);
+            usedSpawns.Add(random);
+            random = Random.Range(0, shelfSpawns.Count);
+
+            int j = -1;
+            while (usedSpawns.Contains(random)) //makes sure tasks dont spawn on each other
+            {
+                random++;
+                j++;
+                if (random > shelfSpawns.Count - 1)
+                {
+                    random = 0; //makes sure random doesnt go outside list
+                }
+
+                if (j >= usedSpawns.Count)
+                {
+                    break;
+                }
+                //probably should find different way to do this later
+            }
+        }
     }
 }
