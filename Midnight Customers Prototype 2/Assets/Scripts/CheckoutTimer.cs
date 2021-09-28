3ffd21bccf253c71d3403b8a40d66200265d99c4
cjw20 <cjw20@iu.edu>
2021-09-28 16:24:16 -0400

    fixed timer continuing to run after customer leaves

--- Midnight Customers Prototype 2/Assets/Scripts/CheckoutTimer.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/CheckoutTimer.cs
@@ -56,6 +56,7 @@ public class CheckoutTimer : MonoBehaviour
                         }
 
                         ResetTimer();
+                        return;
                         //leave!
                     }
                 }
