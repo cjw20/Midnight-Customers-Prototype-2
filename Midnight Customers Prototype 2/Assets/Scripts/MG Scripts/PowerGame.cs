d6f4b439ca5bf1efb7e7f48bbd200f43cf0f9ef4
cjw20 <cjw20@iu.edu>
2021-11-09 14:10:39 -0500

    Created prefabs for trigger and  power minigame. Finished game in basic form
    has same bug as stocking minigame, can only click on certain buttons

--- Midnight Customers Prototype 2/Assets/Scripts/MG Scripts/PowerGame.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/MG Scripts/PowerGame.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PowerGame : MonoBehaviour
 {
+    [SerializeField] PowerButton[] offButtons;
+    [SerializeField] PowerButton[] onButtons;
+
+    MiniGameControl mgControl;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mgControl = FindObjectOfType<MiniGameControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CheckFinish()
+    {
+        bool finished = true;
+        foreach(PowerButton button in onButtons)
+        {
+            if (!button.activated)
+            {
+                finished = false;
+            }
+        }
+
+        if (finished)
+        {
+            mgControl.EndMiniGame();
+            Destroy(this.gameObject);
+        }
     }
 }
