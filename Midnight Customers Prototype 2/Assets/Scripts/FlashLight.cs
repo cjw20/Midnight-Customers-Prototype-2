5883213601fb2dbb5113ec1f1aaaf0a9884b316d
cjw20 <cjw20@iu.edu>
2021-11-09 12:35:03 -0500

    flashlight now follows mouse position on screen so that player can freely look around

--- Midnight Customers Prototype 2/Assets/Scripts/FlashLight.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/FlashLight.cs
@@ -9,6 +9,7 @@ public class FlashLight : MonoBehaviour
     bool on = false;
     [SerializeField] Light2D fLight;
 
+    [SerializeField] float rotateSpeed;
     private void Awake()
     {
         playerInput = new PlayerInput();
@@ -31,9 +32,23 @@ public class FlashLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!on)
+            return;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = new Vector3(mousePos.x, mousePos.y, 0); // ignores z position of mouse so that flashlight stays in 2d plane
+        
+        Vector3 targetDirection = mousePos - transform.position;
+
+
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+        angle -= 90; //dont know why, but above function is off by 90 degrees so this fixes it
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+
+
         
     }
-
     public void ToggleFlashlight()
     {
         on = !on;
