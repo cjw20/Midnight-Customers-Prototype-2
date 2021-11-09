a3f6ec36ae59b690698db53d4488db3971ad720d
Stephen <nixon.stephen1@gmail.com>
2021-11-09 16:20:24 -0500

    Fixed regression issue where dirt didn't appear

--- Midnight Customers Prototype 2/Assets/Scripts/PlayerTrigger.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/PlayerTrigger.cs
@@ -11,16 +11,19 @@ public class PlayerTrigger : MonoBehaviour
     public GameObject mopTriggerPrefab;
     bool hasPlacedMop = false;
     bool isBehindCounter = false;
-    public float poopChance = 0f; //Currently inert code
+    public float poopChance = 0.4f; //Currently inert code
     void OnTriggerEnter2D(Collider2D other){
         if (other.tag=="Register"){
             isBehindCounter=true;
         }
         if (other.tag=="Customer" && !hasPlacedMop && !isBehindCounter){
             float randy = Random.Range(0f,1f);
-            if(randy <= poopChance) Instantiate(mopTriggerPrefab, transform.position, Quaternion.identity);
-            //Add oof sfx?
-            hasPlacedMop = true;
+            Debug.Log(randy);
+            if(randy <= poopChance) {
+                Instantiate(mopTriggerPrefab, transform.position, Quaternion.identity);
+                //Add oof sfx?
+                hasPlacedMop = true;
+            }
         }
     }
     void OnTriggerExit2D(Collider2D other){
