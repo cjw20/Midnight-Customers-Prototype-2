0608fb0831107d6b51a1dfd6ad1392f99078dc75
cjw20 <cjw20@iu.edu>
2021-11-16 14:05:10 -0500

    Deleted some unused sprites and started setting up animations

---
+++ Midnight Customers Prototype 2/Assets/Scripts/AnimControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour
+{
+    Rigidbody2D body;
+    public Vector2 velocity;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = this.gameObject.GetComponent<Rigidbody2D>();
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = body.velocity;
+        animator.SetFloat("Horizontal", velocity.x);
+        animator.SetFloat("Vertical", velocity.y);
+
+        if(Mathf.Abs(velocity.x) > 0 || Mathf.Abs(velocity.y) > 0)
+        {
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+            //probably better way to do this, but oh well
+        }
+    }
+}
