8c94ed856adba474ee8a60114d95b45c0157d9bf
Robbie Frank <32827204+Annuvian@users.noreply.github.com>
2021-12-29 11:23:31 -0500

    adds headers and tooltips to the PlayerTrigger class

--- Midnight Customers Prototype 2/Assets/Scripts/PlayerTrigger.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/PlayerTrigger.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerTrigger : MonoBehaviour
 {
-    //Get reference to mop prefab
-    //implement trigger, add behaviors later
-    //instantiate at customer location (maybe should put in CustomerMovement)
-    public GameObject mopTriggerPrefab;
+    // Fields
     bool hasPlacedMop = false;
     bool isBehindCounter = false;
-
+    [Header("Trigger Data")]
+    [Tooltip("Not implemented.")]
     public float poopChance = 0.4f; //Currently inert code
+
+    // References
+    [Header("Trigger References")]
+    [Tooltip("Reference to a Mop Trigger Prefab.")]
+    public GameObject mopTriggerPrefab;
+    // implement trigger, add behaviors later
+    // instantiate at customer location (maybe should put in CustomerMovement)
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -22,9 +27,9 @@ public class PlayerTrigger : MonoBehaviour
             float randy = Random.Range(0f,1f);
             //Debug.Log(randy);
             if(randy <= poopChance) {
-                //I'm going to leave this commented out
-                //Instantiate(mopTriggerPrefab, transform.position, Quaternion.identity);
-                //Add oof sfx?
+                // I'm going to leave this commented out
+                // Instantiate(mopTriggerPrefab, transform.position, Quaternion.identity);
+                // Add oof sfx?
                 hasPlacedMop = true;
             }
         }
