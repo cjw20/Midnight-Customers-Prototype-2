73db3a52ebd04f303edb108e2636f11e16f6c0ba
Stephen <nixon.stephen1@gmail.com>
2022-01-19 14:19:01 -0500

    initial attempts at a menu

---
+++ Midnight Customers Prototype 2/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (GameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    void Pause(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Resume(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
