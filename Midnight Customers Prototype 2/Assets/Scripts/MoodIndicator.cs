87ace76f6b7f99f680fd0ea4e59ee3e6c30486c1
cjw20 <cjw20@iu.edu>
2022-03-01 12:27:08 -0500

    Mood indicator now hides after 3 seconds from mood change
    I tried to set up fade effect for it but it wasn't working for some reason

--- Midnight Customers Prototype 2/Assets/Scripts/MoodIndicator.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/MoodIndicator.cs
@@ -21,6 +21,9 @@ public class MoodIndicator : MonoBehaviour
     private void Awake()
     {
         spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        StartCoroutine(Hide());
+        //spriteRenderer.color = new Color(1f, 1f, 1f, 1f); //resets sprite from fade out
+        //HideEmote();
     }
 
     public void SetMood(string mood)
@@ -28,20 +31,54 @@ public class MoodIndicator : MonoBehaviour
         switch (mood)
         {
             case "happy":
+                spriteRenderer.enabled = true;
                 spriteRenderer.sprite = happySprite;
+                StartCoroutine(Hide());
+                //spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+                //HideEmote();
                 break;
             case "sad":
+                spriteRenderer.enabled = true;
                 spriteRenderer.sprite = sadSprite;
+                StartCoroutine(Hide());
+                //spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+                //HideEmote();
                 break;
             case "angry":
+                spriteRenderer.enabled = true;
                 spriteRenderer.sprite = angrySprite;
+                StartCoroutine(Hide());
+                //spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+                //HideEmote();
                 break;
             case "pissed":
+                spriteRenderer.enabled = true;
                 spriteRenderer.sprite = pissedSprite;
+                StartCoroutine(Hide());
+                //spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+                //HideEmote();
                 break;
             default:
+                spriteRenderer.enabled = true;
                 spriteRenderer.sprite = happySprite;  //default to happy just in case
+                StartCoroutine(Hide());
+                //spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+                //HideEmote();
                 break;
         }
+    }
+
+    void HideEmote()
+    {
+        SpriteFade fade = this.gameObject.AddComponent<SpriteFade>();
+        fade.FadeOut(1f);
+        Destroy(fade);
+    }
+
+    IEnumerator Hide()
+    {
+        yield return new WaitForSeconds(3f);
+        spriteRenderer.enabled = false;
+        yield break;
     }
 }
