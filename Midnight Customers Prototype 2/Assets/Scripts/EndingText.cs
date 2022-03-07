02a27cfcdd22730f322fe746603363e60cb4dfc4
cjw20 <cjw20@iu.edu>
2022-03-07 12:53:58 -0500

    Added ending text and updated contact names

---
+++ Midnight Customers Prototype 2/Assets/Scripts/EndingText.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingText : MonoBehaviour
+{
+    [TextArea(3, 10)]
+    public string[] endingContents;
+}
