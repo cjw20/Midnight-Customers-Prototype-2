3f93b13de68f430e7a25bacd4ad35939f99e41b9
cjw20 <cjw20@iu.edu>
2022-03-09 15:51:58 -0500

    Created scroll effect for credits

---
+++ Midnight Customers Prototype 2/Assets/Scripts/ScrollEffect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScrollEffect : MonoBehaviour
+{
+    public bool scrolling;
+    public RectTransform rect;
+    public float scrollRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrolling = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scrolling)
+        {
+            rect.anchoredPosition += new Vector2(0, scrollRate * Time.deltaTime);
+        }
+    }
+}
