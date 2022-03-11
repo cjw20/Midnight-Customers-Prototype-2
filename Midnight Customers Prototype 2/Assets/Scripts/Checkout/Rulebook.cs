3df4d0bf4eaf9055b90835f607aa7c94da22c477
Stephen <nixon.stephen1@gmail.com>
2022-03-10 21:58:25 -0500

    Actually implemented new journal assets

--- Midnight Customers Prototype 2/Assets/Scripts/Checkout/Rulebook.cs
+++ Midnight Customers Prototype 2/Assets/Scripts/Checkout/Rulebook.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rulebook : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class Rulebook : MonoBehaviour
     public GameObject openedBook;
     [Tooltip("Reference to the SoundManager.")]
     [SerializeField] SoundManager soundManager;
+    [SerializeField] Sprite image_one;
+    [SerializeField] Sprite image_two;
+    [SerializeField] Sprite image_three;
+    [SerializeField] Sprite image_four;
+    int index = 0;
     
     public void ToggleBook()
     {
@@ -33,9 +39,32 @@ public class Rulebook : MonoBehaviour
         openedBook.SetActive(bookStatus);
     }
     
-    public void CloseBook(){
-        bookStatus = false;
-        openedBook.SetActive(bookStatus);
+    public void NextPage(){
+
+        Image current_image = openedBook.GetComponent<Image>();
+        switch(index){
+            case 0: 
+                current_image.sprite = image_two;
+                break;
+            case 1:
+                current_image.sprite = image_three;
+                break;
+            case 2:
+                current_image.sprite = image_four;
+                break;
+            case 3:
+                current_image.sprite = image_one;
+                index=-1;
+                openedBook.SetActive(false);
+                break;
+
+            default:
+                break;
+        }
+        index++;
+        //set page
+        //if page is last, close
+        //image references
     }
 
     private void OnMouseUp()
